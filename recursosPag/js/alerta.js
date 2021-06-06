61a90e754910eb7c8acb910baf29cfc6d6b20a52
maximiliano argomedo <max.argomedo@duocuc.cl>
2021-06-06 18:46:51 -0400

    Segunda revisión de nuestro proyecto.

---
+++ recursosPag/js/alerta.js
@@ -0,0 +1,10 @@
+$('#btnEnviar').click(function() {
+    window.alert("Mensaje fue enviado correctamente")
+    
+});
+
+$('#btnEnviar2').click(function() {
+    window.alert("Te has registrado correctamente")
+    window.alert("Te llegara un correo de confirmación.")
+    
+});
